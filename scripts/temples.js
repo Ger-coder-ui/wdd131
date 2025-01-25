42b416487e350f3e8a9ba6ecee3fbb6a444e196e
Ger - coder - ui <geraldmango3@gmail.com>
2025-01-25 04:31:55 +0200

    css

--- scripts/temples.js
+++ scripts/temples.js
@@ -12,3 +12,27 @@ document.getElementById("currentyear").textContent = currentYear;
 
 // Update the last modified date in the footer
 document.getElementById("lastModified").textContent = `Last modified: ${formattedLastModifiedDate}`;
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('show');
+});
+
+// Add event listener to close hamburger menu when clicking outside
+document.addEventListener('click', (e) => {
+  if (e.target !== hamburger && e.target !== navMenu) {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('show');
+  }
+});
+
+// Add event listener to close hamburger menu when resizing window
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('show');
+  }
+});
